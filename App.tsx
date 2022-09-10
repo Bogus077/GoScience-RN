957f72ffa7977dc388bdb4f27ceb6858e56ca955
Bogus077 <bogus077@gmail.com>
2022-09-10 23:57:59 +0400

    feat/add linter and prettier

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import logo from './assets/logo192.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Image source={logo} style={{ width: 192, height: 192 }} />
+      <Text style={{ color: '#000' }}>Hello, World!!!</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={{ backgroundColor: 'blue' }}
+      >
+        <Text style={{ fontSize: 20, color: '#fff' }}>Pick a photo!!</Text>
+      </TouchableOpacity>
     </View>
   );
 }
